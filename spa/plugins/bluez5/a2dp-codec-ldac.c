9c4e6db9dc2cf7a91ac8727868775ec3dc98b136
ZZyVSmOzMz OZaAEBlUIZ <1080187-ZZyVSmOzMz@users.noreply.gitlab.freedesktop.org>
2025-06-22 12:30:53 +0000

    check version

--- spa/plugins/bluez5/a2dp-codec-ldac.c
+++ spa/plugins/bluez5/a2dp-codec-ldac.c
@@ -471,7 +471,11 @@ static void *codec_init(const struct media_codec *codec, uint32_t flags,
 #endif
 
 #ifdef ENABLE_LDAC_ABR
-	this->ldac_abr = ldac_ABR_get_handle(NULL);
+#ifdef ENABLE_LDAC_DEC
+		this->ldac_abr = ldac_ABR_get_handle(NULL);
+#else
+		this->ldac_abr = ldac_ABR_get_handle();
+#endif
 	if (this->ldac_abr == NULL)
 		goto error_errno;
 #endif
